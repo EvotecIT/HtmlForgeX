6c00af527921ec01603279e863a774d53422777d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-13 23:41:05 +0200

    feat: Add FontVariant enum for representing font variants

---
+++ HtmlForgeX/Enums/FontVariant.cs
@@ -0,0 +1,16 @@
+namespace HtmlForgeX;
+
+public enum FontVariant {
+    Normal = 1,
+    SmallCaps
+}
+
+public static class FontVariantExtensions {
+    public static string ToCssString(this FontVariant value) {
+        return value switch {
+            FontVariant.Normal => "normal",
+            FontVariant.SmallCaps => "small-caps",
+            _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+        };
+    }
+}
