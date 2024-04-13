f85b441cdd9f6b62db9a3dce534c0b9356b9a752
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-13 23:38:26 +0200

    refactor: Add ExtensionsDescription class for retrieving descriptions of enum values

---
+++ HtmlForgeX/Extensions/ExtensionsDescription.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel;
+
+namespace HtmlForgeX.Extensions;
+
+internal static class ExtensionsDescription {
+    internal static string GetDescription<T>(this T enumValue) where T : IConvertible {
+        if (!typeof(T).IsEnum) {
+            throw new ArgumentException("Argument must be of type Enum");
+        }
+
+        var description = enumValue.ToString();
+        var fieldInfo = enumValue.GetType().GetField(description);
+        var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        if (attributes != null && attributes.Length > 0) {
+            description = attributes[0].Description;
+        }
+
+        return description;
+    }
+}
