c7928e70ac29d8ff3290af4681fa1ad5ab7a3719
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-13 23:40:38 +0200

    ```text feat: Add Display enum for representing CSS display values
    This commit adds the Display enum to the HtmlForgeX.Enums namespace. The enum includes various values that represent different CSS display property values, such as "none", "inline", "block", and more. This addition enhances the functionality of the HtmlForgeX library by providing a convenient way to specify display styles for HTML elements.

---
+++ HtmlForgeX/Enums/Display.cs
@@ -0,0 +1,77 @@
+using System.ComponentModel;
+
+namespace HtmlForgeX;
+
+public enum Display {
+    [Description("none")]
+    None = 1,
+
+    [Description("inline")]
+    Inline,
+
+    [Description("block")]
+    Block,
+
+    [Description("inline-block")]
+    InlineBlock,
+
+    [Description("contents")]
+    Contents,
+
+    [Description("flex")]
+    Flex,
+
+    [Description("grid")]
+    Grid,
+
+    [Description("inline-flex")]
+    InlineFlex,
+
+    [Description("inline-grid")]
+    InlineGrid,
+
+    [Description("inline-table")]
+    InlineTable,
+
+    [Description("list-item")]
+    ListItem,
+
+    [Description("run-in")]
+    RunIn,
+
+    [Description("table")]
+    Table,
+
+    [Description("table-caption")]
+    TableCaption,
+
+    [Description("table-column-group")]
+    TableColumnGroup,
+
+    [Description("table-header-group")]
+    TableHeaderGroup,
+
+    [Description("table-footer-group")]
+    TableFooterGroup,
+
+    [Description("table-row-group")]
+    TableRowGroup,
+
+    [Description("table-cell")]
+    TableCell,
+
+    [Description("table-column")]
+    TableColumn,
+
+    [Description("table-row")]
+    TableRow
+}
+
+public static class DisplayExtensions {
+    public static string ToCssString(this Display value) {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+        var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        return attributes.Length > 0 ? attributes[0].Description : value.ToString().ToLower();
+    }
+}
