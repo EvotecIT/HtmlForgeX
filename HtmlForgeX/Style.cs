feef735181509ce29d348222c36ef6cbd2221302
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-13 22:56:38 +0200

    Add style

---
+++ HtmlForgeX/Style.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlForgeX;
+
+public class Style {
+    public string Selector { get; }
+    public Dictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+
+    public Style(string selector) {
+        Selector = selector;
+    }
+
+    public Style Add(string property, string value) {
+        Properties[property] = value;
+        return this;
+    }
+
+    public override string ToString() {
+        var properties = string.Join("; ", Properties.Select(kvp => $"{kvp.Key}: {kvp.Value}"));
+        return $"{Selector} {{ {properties} }}";
+    }
+}
