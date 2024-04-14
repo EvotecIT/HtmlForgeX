c2bfae78b27368719cb38a486e6d7feddfd200d7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-14 16:37:44 +0200

    Files for storage

---
+++ HtmlForgeX/Support/GlobalStorage.cs
@@ -0,0 +1,18 @@
+﻿using HtmlForgeX.Resources;
+
+namespace HtmlForgeX;
+
+internal static class GlobalStorage {
+    internal static LibraryMode LibraryMode { get; set; } = LibraryMode.Online;
+    internal static HashSet<Libraries> Libraries { get; } = new HashSet<Libraries>();
+    internal static List<string> Errors { get; } = new List<string>();
+
+
+    internal static string GenerateRandomId(string preText) {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        var randomId = new string(Enumerable.Repeat(chars, 5)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+        return $"{preText}-{randomId}";
+    }
+}
---
+++ HtmlForgeX/Support/Library.cs
@@ -0,0 +1,66 @@
+using HtmlForgeX.Resources;
+
+namespace HtmlForgeX;
+
+public class Library {
+    public string Comment { get; set; }
+    public LibraryLinks Header { get; set; }
+    public LibraryLinks HeaderAlways { get; set; }
+    public LibraryLinks Footer { get; set; }
+    public LibraryLinks FooterAlways { get; set; }
+    public LibraryLinks Body { get; set; }
+    public string LicenseLink { get; set; }
+    public string License { get; set; }
+    public string SourceCodes { get; set; }
+    public string Website { get; set; }
+    public bool Default { get; set; }
+    public bool Email { get; set; }
+}
+
+public class LibraryLinks {
+    public List<string> CssLink { get; set; }
+    public Dictionary<string, Dictionary<string, string>> CssInLine { get; set; }
+    public List<string> Css { get; set; }
+    public List<string> JSLinkOriginal { get; set; }
+    public List<string> JsLink { get; set; }
+    public List<string> JS { get; set; }
+}
+
+public class LibrariesConverter {
+    public static Library MapLibraryEnumToLibraryObject(Libraries libraries) {
+        switch (libraries) {
+            case Libraries.Bootstrap:
+                return new Bootstrap();
+            case Libraries.DataTables:
+                return new DataTables();
+            case Libraries.Tabler:
+                return new Tabler();
+            case Libraries.JQuery:
+                return new Jquery();
+            default:
+                throw new ArgumentException($"Unsupported library: {libraries}");
+        }
+        return null;
+    }
+}
+
+public enum LibraryMode {
+    Online,
+    Offline,
+    OfflineWithFiles
+}
+
+public enum TableType {
+    None,
+    BootstrapTable,
+    DataTables,
+    Tabler
+}
+
+public enum Libraries {
+    None,
+    Bootstrap,
+    DataTables,
+    Tabler,
+    JQuery
+}
