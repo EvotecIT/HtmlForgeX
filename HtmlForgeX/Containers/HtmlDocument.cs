b61cc36f606ab26f3c59d71e97d7a04de6806c58
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-14 16:38:58 +0200

    refactor: Inherit HtmlBody class from HtmlElementContainer

--- HtmlForgeX/Containers/HtmlDocument.cs
+++ HtmlForgeX/Containers/HtmlDocument.cs
@@ -1,21 +1,33 @@
-using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace HtmlForgeX;
 
-public class HtmlDocument {
+public class HtmlDocument : HtmlElementContainer {
     public HtmlHead Head = new HtmlHead();
     public HtmlBody Body = new HtmlBody();
 
-    public HtmlDocument() {
+    public LibraryMode LibraryMode {
+        get => GlobalStorage.LibraryMode;
+        set => GlobalStorage.LibraryMode = value;
+    }
 
+    public HtmlDocument(LibraryMode? librariesMode = null) {
+        if (librariesMode != null) {
+            GlobalStorage.LibraryMode = librariesMode.Value;
+        }
     }
 
     public void Save(string path, bool openInBrowser = false) {
+        var countErrors = GlobalStorage.Errors.Count;
+        if (countErrors > 0) {
+            Console.WriteLine($"There were {countErrors} found during generation of HTML.");
+            //foreach (var error in GlobalStorage.Errors) {
+            //    Console.WriteLine(error);
+            //}
+        }
+
         System.IO.File.WriteAllText(path, this.ToString());
         Helpers.Open(path, openInBrowser);
-
     }
 
     public override string ToString() {
@@ -28,5 +40,31 @@ public class HtmlDocument {
         html.AppendLine("</html>");
 
         return html.ToString();
+    }
+
+    public void AddLibrary(Libraries library) {
+        GlobalStorage.Libraries.Add(library);
+    }
+
+    public void AddLibrary(Library library) {
+        if (GlobalStorage.LibraryMode == LibraryMode.Online) {
+            foreach (var link in library.Header.CssLink) {
+                this.Head.AddCssLink(link);
+            }
+
+            foreach (var link in library.Header.JsLink) {
+                this.Head.AddJsLink(link);
+            }
+        } else if (GlobalStorage.LibraryMode == LibraryMode.Offline) {
+            foreach (var css in library.Header.Css) {
+                var cssContent = System.IO.File.ReadAllText(css);
+                this.Head.AddCssInline(cssContent);
+            }
+
+            foreach (var js in library.Header.JS) {
+                var jsContent = System.IO.File.ReadAllText(js);
+                this.Head.AddJsInline(jsContent);
+            }
+        }
     }
 }
