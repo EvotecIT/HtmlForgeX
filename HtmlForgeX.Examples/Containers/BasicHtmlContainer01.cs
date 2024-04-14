82e802b41f0803f36479a8cfa3c9712350e78d7a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-14 18:20:25 +0200

    More code

--- HtmlForgeX.Examples/Containers/BasicHtmlContainer01.cs
+++ HtmlForgeX.Examples/Containers/BasicHtmlContainer01.cs
@@ -10,6 +10,25 @@ internal class BasicHtmlContainer01 {
     public static void Demo01(bool openInBrowser = false) {
         HelpersSpectre.PrintTitle("Basic Demo Document Container 1");
 
+        // Create a list of simple objects
+        var data = new List<dynamic> {
+            new { Name = "John", Age = 30, Occupation = "Engineer" },
+            new { Name = "Jane", Age = 28, Occupation = "Doctor" },
+            new { Name = "Bob", Age = 35, Occupation = "Architect" }
+        };
+
+        var data1 = new List<dynamic> {
+            new { Name = "John", Age = 30, Occupation = "Engineer" },
+            new { Name = "Jane", Age = 28, Occupation = "Doctor" },
+            new { Name = "Bob", Age = 35, Occupation = "Architect" },
+            new { Name = "John", Age = 30, Occupation = "Engineer" },
+        };
+
+        var data2 = new List<dynamic> {
+            new { Name = "John", Age = 30, Occupation = "Engineer" },
+            new { Name = "Jane", Age = 28, Occupation = "Doctor" },
+        };
+
         HtmlDocument document = new HtmlDocument();
         document.Head.Title = "Basic Demo Document 1";
         document.Head.Author = "Przemysław Kłys";
@@ -23,7 +42,35 @@ internal class BasicHtmlContainer01 {
         document.Head.AddViewportMeta("width=device-width, initial-scale=1.0");
 
 
-        document.Body.Add(new HtmlLayout().ToString());
+        // Add the table to the document
+        document.Body.AddTable(data, TableType.Tabler);
+
+        var page = new HtmlPage()
+            .Add(new HtmlRow()
+                .Add(new HtmlColumn().WithClass("col-6")
+                    .Add(new HtmlCard().AddContent("Card 1").WithStyle("background-color: red;"))
+                    .Add(new HtmlCard().AddContent("Card 2").WithStyle("background-color: blue;"))
+                )
+                .Add(new HtmlColumn().WithClass("col-6")
+                    .Add(new HtmlCard().AddContent("Card 3").WithStyle("background-color: green;"))
+                    .Add(new HtmlCard().AddTable(data1, TableType.DataTables))
+                )
+            );
+
+
+        document.Body.Add(page.ToString());
+
+        var page1 = new HtmlPage()
+            .Add(new HtmlCard().AddContent("Card 1").WithStyle("background-color: red;"))
+            .Add(new HtmlCard().AddContent("Card 2").WithStyle("background-color: blue;"))
+            .Add(new HtmlCard())
+            .Add(new HtmlCard().AddTable(data2, TableType.DataTables))
+            .Add(new HtmlCard().AddContent("Card 5"));
+
+        document.Body.Add(page1.ToString());
+
+
+
 
 
         document.Save("BasicDemoDocumentContainer01.html", openInBrowser);
--- HtmlForgeX/Containers/HtmlElement.cs
+++ HtmlForgeX/Containers/HtmlElement.cs
@@ -3,8 +3,9 @@ namespace HtmlForgeX;
 public abstract class HtmlElement {
     public List<HtmlElement> Children { get; } = new List<HtmlElement>();
 
-    public void Add(HtmlElement child) {
+    public HtmlElement Add(HtmlElement child) {
         Children.Add(child);
+        return this;
     }
 
     public abstract override string ToString();
