fe9ad530b98d07354c5731b7d3e267f890f5242e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-18 23:44:26 +0200

    Style improvement

--- HtmlForgeX/Styles/Style.cs
+++ HtmlForgeX/Styles/Style.cs
@@ -1,11 +1,20 @@
 namespace HtmlForgeX;
 
 public class Style {
-    public string Selector { get; }
-    public Dictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+    public string Selector { get; set; }
+    public Dictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
 
     public Style(string selector) {
         Selector = selector;
+    }
+
+    public Style(string selector, Dictionary<string, string> properties) {
+        Selector = selector;
+        Properties = properties;
+    }
+
+    public Style() {
+
     }
 
     public Style Add(string property, string value) {
