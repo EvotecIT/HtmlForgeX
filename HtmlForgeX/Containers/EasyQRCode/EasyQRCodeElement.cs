12be39b76d220ea9212d9eb9c64ee25f233f31a9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-19 19:23:14 +0200

    feat: Add EasyQRCodeElement class for QR code container

---
+++ HtmlForgeX/Containers/EasyQRCode/EasyQRCodeElement.cs
@@ -0,0 +1,25 @@
+namespace HtmlForgeX;
+
+public class EasyQRCodeElement : HtmlElement {
+    public string Id { get; set; }
+    public string Text { get; set; }
+
+    public EasyQRCodeElement(string text) {
+        GlobalStorage.Libraries.Add(Libraries.EasyQRCode);
+        Id = GlobalStorage.GenerateRandomId("QrCode");
+        Text = text;
+    }
+
+    public override string ToString() {
+        var divTag = new HtmlTag("div").Class("qrcode").Id(Id);
+
+        var scriptTag = new HtmlTag("script").SetValue($@"
+            var options = {{
+                ""text"": ""{Text}""
+            }};
+            new QRCode(document.getElementById(""{Id}""), options);
+        ");
+
+        return divTag + scriptTag.ToString();
+    }
+}
