ba2acd90ccf2d40f454d2fe692ef0470b1ea7578
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-20 08:28:35 +0200

    Refactor BasicHtmlContainer03 to use DataTablesTable with striped style

--- HtmlForgeX.Examples/Containers/BasicHtmlContainer03.cs
+++ HtmlForgeX.Examples/Containers/BasicHtmlContainer03.cs
@@ -163,7 +163,7 @@ internal class BasicHtmlContainer03 {
                         card.Span("This is inside card").AddContent(" with some color").WithColor(RGBColor.Amber);
                         card.LineBreak();
                         card.Span("This is continuing after").AppendContent(" linebreak ").WithColor(RGBColor.RedDevil).AppendContent(" cool?");
-                        var table1 = (DataTablesTable)card.Table(data, TableType.DataTables);
+                        var table1 = ((DataTablesTable)card.Table(data, TableType.DataTables)).Style(BootStrapTableStyle.Striped);
                         table1.EnableOrdering = true;
                         table1.EnableSearching = true;
                         table1.EnableScrollX = true;
