6406beb11c8b367bb5fe2b28c0d8f7bebc9f885d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-20 08:28:23 +0200

    ```text feat: Refactor TablerTable class to use HtmlTag for building the table
    This commit refactors the TablerTable class to use the HtmlTag class for building the table. It adds an `Id` property to the TablerTable class and modifies the `BuildTable` method to use the HtmlTag to generate the table HTML. This change improves code structure and readability, and allows for more flexibility in customizing the table.

--- HtmlForgeX/Containers/Tabler/TablerTable.cs
+++ HtmlForgeX/Containers/Tabler/TablerTable.cs
@@ -3,31 +3,18 @@ using System.Text;
 namespace HtmlForgeX;
 
 public class TablerTable : HtmlTable {
+    public string Id;
     public TablerTable(IEnumerable<object> objects, TableType library) : base(objects, library) {
-        // Additional initialization code specific to Tabler...
+
     }
 
-    public void EnableDarkMode() {
-        // Enable dark mode for Tabler tables
-    }
-
-    public void EnableCompactMode() {
-        // Enable compact mode for Tabler tables
-    }
-
-    // Override the BuildTable method to use these modes
     public override string BuildTable() {
-        StringBuilder html = new StringBuilder();
-        html.AppendLine("<table class=\"table\">");
-
-        // Use the base class's BuildTable method, but add the Tabler modes
-        html.AppendLine(base.BuildTable());
-        // Add the Tabler modes to the table string
-
-
-
-
-        html.AppendLine("</table>");
-        return html.ToString();
+        string tableInside = base.BuildTable();
+        string classNames = "table";
+        var tableTag = new HtmlTag("table")
+            .Id(Id)
+            .Class(classNames)
+            .SetValue(tableInside);
+        return tableTag.ToString();
     }
 }
