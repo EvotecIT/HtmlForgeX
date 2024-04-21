8b4c78df0774a31adb42dd33e6219887a4938e48
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-21 08:56:23 +0200

    Add icons

---
+++ HtmlForgeX/Resources/TablerIconLibrary.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlForgeX.Resources;
+internal class TablerIconLibrary : Library {
+    public TablerIconLibrary() {
+        Header = new LibraryLinks {
+            CssLink = ["https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"],
+            Css = ["tabler-icons.min.css"],
+            JsLink = [],
+            Js = []
+        };
+        Comment = "Tabler Icon library";
+        LicenseLink = "https://github.com/tabler/tabler-icons/blob/main/LICENSE";
+        License = "MIT";
+        Website = "https://tablericons.com/";
+        SourceCodes = "https://github.com/tabler/tabler-icons";
+        Default = true;
+        Email = false;
+    }
+}
