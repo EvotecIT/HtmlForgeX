8b2b64e4ad9e7d60ae0bd357a9ce0e66990ba2be
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-21 08:43:46 +0200

    Add TablerCardMini

---
+++ HtmlForgeX/Containers/Tabler/TablerCardMini.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlForgeX;
+public class TablerCardMini : TablerCard {
+    private string? CardStyle { get; set; }
+
+    private TablerIcon AvatarIcon { get; set; } = TablerIcon.User;
+    private BadgeColor AvatarBackgroundColor { get; set; } = BadgeColor.Blue;
+    private BadgeColor AvatarTextColor { get; set; } = BadgeColor.White;
+    private string TitleText { get; set; } = "";
+    private string SubtitleText { get; set; } = "";
+
+
+    public TablerCardMini Avatar(TablerIcon icon) {
+        AvatarIcon = icon;
+        return this;
+    }
+
+    public TablerCardMini BackgroundColor(BadgeColor color) {
+        AvatarBackgroundColor = color;
+        return this;
+    }
+
+    public TablerCardMini TextColor(BadgeColor color) {
+        AvatarTextColor = color;
+        return this;
+    }
+
+    public TablerCardMini Title(string title) {
+        TitleText = title;
+        return this;
+    }
+
+    public TablerCardMini Subtitle(string subtitle) {
+        SubtitleText = subtitle;
+        return this;
+    }
+
+    public override string ToString() {
+        //Console.WriteLine("Generating HtmlCard...");
+
+        // Create the outer div for the card
+        var cardDiv = new HtmlTag("div");
+        cardDiv.Class("card").Class(CardStyle).Class(null);
+
+        // Create the inner div for the card body
+        var cardBodyDiv = new HtmlTag("div");
+        cardBodyDiv.Class("card-body").Append(Content);
+        cardBodyDiv.Attributes["style"] = Style;
+
+        var cardInside = cardBodyDiv.Row(cardRow => {
+            cardRow.Column(TablerColumnNumber.Auto, avatarColumn => {
+                avatarColumn.Avatar().Icon(AvatarIcon).BackgroundColor(AvatarBackgroundColor)
+                    .TextColor(AvatarTextColor);
+
+            });
+            cardRow.Column(textColumn => {
+                textColumn.Text(TitleText).Weight(TablerFontWeight.Medium);
+                textColumn.Text(SubtitleText).Style(TableTextStyle.Muted);
+            });
+        });
+
+        cardBodyDiv.Append(cardInside);
+
+        // Add the card body to the card
+        cardDiv.Append(cardBodyDiv);
+
+        var result = cardDiv.ToString();
+
+        // Console.WriteLine("Generated TablerCardMini: " + result);
+        return result;
+    }
+}
