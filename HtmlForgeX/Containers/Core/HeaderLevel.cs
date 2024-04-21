d970657893eda4a19ce32363f4eb812ef1a021ae
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-21 22:11:35 +0200

    Build header level

--- HtmlForgeX/Containers/Core/HeaderLevel.cs
+++ HtmlForgeX/Containers/Core/HeaderLevel.cs
@@ -1,7 +1,7 @@
 namespace HtmlForgeX;
 
 public class HeaderLevel : Element {
-    private string PrivateTag { get; }
+    private HeaderLevelTag PrivateTag { get; }
 
     private string? PrivateText { get; }
 
@@ -12,13 +12,9 @@ public class HeaderLevel : Element {
     /// </summary>
     /// <param name="level">The level.</param>
     /// <param name="text">The text.</param>
-    public HeaderLevel(HeaderLevel level, string text) {
-        PrivateTag = level.PrivateTag;
+    public HeaderLevel(HeaderLevelTag level, string text) {
+        PrivateTag = level;
         PrivateText = text;
-    }
-
-    private HeaderLevel(string tag) {
-        PrivateTag = tag;
     }
 
     /// <summary>
@@ -33,17 +29,17 @@ public class HeaderLevel : Element {
     }
 
     public override string ToString() {
-        var tag = new HtmlTag(PrivateTag).SetValue(PrivateText);
+        var tag = new HtmlTag(PrivateTag.ToString()).SetValue(PrivateText);
         if (!string.IsNullOrEmpty(PrivateClass)) {
             tag.Class(PrivateClass);
         }
         return tag.ToString();
     }
 
-    public static HeaderLevel H1 => new HeaderLevel("h1");
-    public static HeaderLevel H2 => new HeaderLevel("h2");
-    public static HeaderLevel H3 => new HeaderLevel("h3");
-    public static HeaderLevel H4 => new HeaderLevel("h4");
-    public static HeaderLevel H5 => new HeaderLevel("h5");
-    public static HeaderLevel H6 => new HeaderLevel("h6");
+    //public static HeaderLevel H1 => new HeaderLevel("h1");
+    //public static HeaderLevel H2 => new HeaderLevel("h2");
+    //public static HeaderLevel H3 => new HeaderLevel("h3");
+    //public static HeaderLevel H4 => new HeaderLevel("h4");
+    //public static HeaderLevel H5 => new HeaderLevel("h5");
+    //public static HeaderLevel H6 => new HeaderLevel("h6");
 }
