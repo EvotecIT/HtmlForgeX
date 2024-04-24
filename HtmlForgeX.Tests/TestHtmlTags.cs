2fbb6902c8ee9aaecb909bf25bb0d0c97f8271f0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-24 22:53:50 +0200

    feat: Add HTML tags for line breaks, headers, and horizontal rule
    This commit adds HTML tags for line breaks, headers (h1-h6), and horizontal rule (hr). It includes the implementation of the corresponding classes and their ToString() methods. These changes enhance the functionality of the HtmlForgeX library and provide more options for creating structured HTML content.

--- HtmlForgeX.Tests/TestHtmlTags.cs
+++ HtmlForgeX.Tests/TestHtmlTags.cs
@@ -35,8 +35,39 @@ public class TestHtmlTags {
         var address = new Address("This is an address.");
         Assert.AreEqual("<address>This is an address.</address>", address.ToString());
 
+        var br = new BR();
+        Assert.AreEqual("<br>", br.ToString());
+
+        var br2 = new LineBreak();
+        Assert.AreEqual("<br>", br2.ToString());
+
+        var h1 = new H1("This is a header 1.");
+        Assert.AreEqual("<h1>This is a header 1.</h1>", h1.ToString());
+
+        var h2 = new H2("This is a header 2.");
+        Assert.AreEqual("<h2>This is a header 2.</h2>", h2.ToString());
+
+        var h3 = new H3("This is a header 3.");
+        Assert.AreEqual("<h3>This is a header 3.</h3>", h3.ToString());
+
+        var h4 = new H4("This is a header 4.");
+        Assert.AreEqual("<h4>This is a header 4.</h4>", h4.ToString());
+
+        var h5 = new H5("This is a header 5.");
+        Assert.AreEqual("<h5>This is a header 5.</h5>", h5.ToString());
+
+        var h6 = new H6("This is a header 6.");
+        Assert.AreEqual("<h6>This is a header 6.</h6>", h6.ToString());
+
+        var hr = new HR();
+        Assert.AreEqual("<hr>", hr.ToString());
+
+        var hr2 = new HorizontalRule();
+        Assert.AreEqual("<hr>", hr2.ToString());
+
         var strong = new Strong("This is a paragraph.");
         Assert.AreEqual("<strong>This is a paragraph.</strong>", strong.ToString());
+
     }
 
 }
