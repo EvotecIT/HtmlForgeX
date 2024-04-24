6e5f0cd2dc66a4b3913758e63729ae787ceca06c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-24 22:20:14 +0200

    add more tags

--- HtmlForgeX/Tags/TagBundle.cs
+++ HtmlForgeX/Tags/TagBundle.cs
@@ -11,4 +11,24 @@ public class Address(string value) : HtmlTag("address", value);
 
 public class Article(string value) : HtmlTag("article", value);
 
+public class BR() : HtmlTag("br", TagMode.NoClosing);
+
+public class LineBreak() : HtmlTag("br", TagMode.NoClosing);
+
+public class HR() : HtmlTag("hr", TagMode.NoClosing);
+
+public class HorizontalRule() : HtmlTag("hr", TagMode.NoClosing);
+
+public class H1(string value) : HtmlTag("h1", value);
+
+public class H2(string value) : HtmlTag("h2", value);
+
+public class H3(string value) : HtmlTag("h3", value);
+
+public class H4(string value) : HtmlTag("h4", value);
+
+public class H5(string value) : HtmlTag("h5", value);
+
+public class H6(string value) : HtmlTag("h6", value);
+
 public class Strong(string value) : HtmlTag("strong", value);
