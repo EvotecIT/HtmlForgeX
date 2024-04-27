10fb7c26bada24159964ca10c940669bd58b8c80
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-27 21:41:17 +0200

    Refactor TablerCardFooter class to add footer functionality

---
+++ HtmlForgeX/Containers/Tabler/TablerCardFooter.cs
@@ -0,0 +1,26 @@
+namespace HtmlForgeX;
+
+public class TablerCardFooter : Element {
+    public string? Content { get; set; }
+
+    public TablerCardFooter SetContent(string content) {
+        Content = content;
+        return this;
+    }
+
+    public override string ToString() {
+        // We only want to render the footer if there are children
+        if (Children.Count > 0) {
+            var footerDiv = new HtmlTag("div");
+            footerDiv.Class("card-footer").Value(Content);
+
+            foreach (var child in Children) {
+                footerDiv.Value(child.ToString());
+            }
+
+            return footerDiv.ToString();
+        }
+        return "";
+
+    }
+}
