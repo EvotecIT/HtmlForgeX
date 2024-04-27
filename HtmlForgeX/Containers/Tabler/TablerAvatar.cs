2fa39d0fa30513df6aa446c72cab6f35fd3ed3eb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-27 21:40:33 +0200

    Refactor TablerAvatar class to add image support and size/margin customization

--- HtmlForgeX/Containers/Tabler/TablerAvatar.cs
+++ HtmlForgeX/Containers/Tabler/TablerAvatar.cs
@@ -4,8 +4,11 @@ namespace HtmlForgeX;
 
 public class TablerAvatar : Element {
     private string ValueEntry { get; set; } = "";
+    private string ImageUrl { get; set; } = "";
     private TablerBadgeColor? ClassBackgroundColor { get; set; }
     private TablerBadgeColor? ClassTextColor { get; set; }
+    private AvatarSize? PrivateAvatarSize { get; set; }
+    private TablerMarginStyle? PrivateAvatarMargin { get; set; }
 
     public TablerAvatar BackgroundColor(TablerBadgeColor color) {
         ClassBackgroundColor = color;
@@ -22,16 +25,36 @@ public class TablerAvatar : Element {
         return this;
     }
 
+    public TablerAvatar Image(string url) {
+        ImageUrl = url;
+        return this;
+    }
+
+    public TablerAvatar Size(AvatarSize size) {
+        PrivateAvatarSize = size;
+        return this;
+    }
+
+    public TablerAvatar Margin(TablerMarginStyle margin) {
+        PrivateAvatarMargin = margin;
+        return this;
+    }
 
     public override string ToString() {
-        HtmlTag avatarTag = new HtmlTag("span").Class($"avatar");
+        HtmlTag avatarTag = new HtmlTag("span").Class("avatar");
+
+        avatarTag.Class(PrivateAvatarSize?.EnumToString());
+        avatarTag.Class(PrivateAvatarMargin?.EnumToString());
+
         if (ClassBackgroundColor != null) {
             avatarTag.Class($"bg-{ClassBackgroundColor.Value.EnumToString()}");
         }
         if (ClassTextColor != null) {
             avatarTag.Class($"text-{ClassTextColor.Value.EnumToString()}");
         }
-        if (!string.IsNullOrEmpty(ValueEntry)) {
+        if (!string.IsNullOrEmpty(ImageUrl)) {
+            avatarTag.Style("background-image", $"url({ImageUrl})");
+        } else if (!string.IsNullOrEmpty(ValueEntry)) {
             avatarTag.Value(ValueEntry);
         }
         return avatarTag.ToString();
