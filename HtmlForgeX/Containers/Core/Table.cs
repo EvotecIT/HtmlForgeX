06625aeac612fc75f7478b0d6631345405afd0b9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-16 14:08:39 +0200

    Improve processing table objects

--- HtmlForgeX/Containers/Core/Table.cs
+++ HtmlForgeX/Containers/Core/Table.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace HtmlForgeX;
@@ -54,10 +55,37 @@ public class Table : Element {
     public Table AddObjects(IEnumerable<object> objects, bool addFooter = false) {
         if (objects == null || !objects.Any()) return this;
 
-        // Get the type of the objects
-        var type = objects.First().GetType();
+        var firstObject = objects.FirstOrDefault();
 
-        // Get the properties of the objects
+        if (firstObject == null) {
+            // Handle the case where the first object is null
+            return this;
+        }
+
+        if (firstObject is IDictionary<string, object>) {
+            AddDictionaryObjects(objects.Cast<IDictionary<string, object>>(), addFooter);
+        } else if (IsStandardObject(firstObject)) {
+            AddStandardObjects(objects, addFooter);
+        } else {
+            AddPsObjectLike(objects, addFooter);
+        }
+
+        return this;
+    }
+
+    private bool IsStandardObject(object obj) {
+        var type = obj.GetType();
+        return type.IsClass && !type.IsPrimitive && !type.IsValueType && type != typeof(string) && !IsDynamicObject(type);
+    }
+
+    private bool IsDynamicObject(Type type) {
+        return type.GetProperty("Members") != null || type.GetProperty("Properties") != null ||
+               type.GetInterfaces().Any(i => i == typeof(System.Dynamic.IDynamicMetaObjectProvider));
+    }
+
+
+    private void AddStandardObjects(IEnumerable<object> objects, bool addFooter) {
+        var type = objects.First().GetType();
         var properties = type.GetProperties();
 
         // Add the headers
@@ -86,9 +114,103 @@ public class Table : Element {
                 AddFooter(property.Name);
             }
         }
-
-        return this;
     }
+
+    private void AddDictionaryObjects(IEnumerable<IDictionary<string, object>> dictObjects, bool addFooter) {
+        // Add the headers
+        foreach (var key in dictObjects.First().Keys) {
+            AddHeader(key);
+        }
+
+        // Add the rows
+        foreach (var dict in dictObjects) {
+            var row = new List<string>();
+            foreach (var key in dict.Keys) {
+                try {
+                    var value = dict[key];
+                    row.Add(value != null ? value.ToString() : "");
+                } catch (Exception ex) {
+                    GlobalStorage.Errors.Add($"Error getting value for key {key}: {ex.Message}");
+                    row.Add("");
+                }
+            }
+            AddRow(row);
+        }
+
+        if (addFooter) {
+            // Add the footers
+            foreach (var key in dictObjects.First().Keys) {
+                AddFooter(key);
+            }
+        }
+    }
+    private bool IsPsObjectLike(object obj) {
+        var type = obj.GetType();
+
+        // Check for properties typically present in a PSObject
+        return type.GetProperty("Properties") != null &&
+               type.GetProperty("Members") != null;
+    }
+
+    private void AddPsObjectLike(IEnumerable<object> objects, bool addFooter) {
+        var firstObject = objects.First();
+
+        if (IsPsObjectLike(firstObject)) {
+            var properties = GetPsObjectProperties(firstObject);
+
+            // Add the headers
+            foreach (var property in properties) {
+                AddHeader(property.Key);
+            }
+
+            // Add the rows
+            foreach (var obj in objects) {
+                var row = new List<string>();
+                foreach (var property in GetPsObjectProperties(obj)) {
+                    row.Add(property.Value?.ToString() ?? "");
+                }
+                AddRow(row);
+            }
+
+            if (addFooter) {
+                // Add the footers
+                foreach (var property in properties) {
+                    AddFooter(property.Key);
+                }
+            }
+        }
+    }
+
+    private IDictionary<string, object> GetPsObjectProperties(object psObject) {
+        var properties = new Dictionary<string, object>();
+
+        var propertiesProperty = psObject.GetType().GetProperty("Properties");
+        if (propertiesProperty != null) {
+            var propertiesValue = propertiesProperty.GetValue(psObject);
+            var enumerable = propertiesValue as IEnumerable;
+
+            if (enumerable != null) {
+                foreach (var item in enumerable) {
+                    var nameProperty = item.GetType().GetProperty("Name");
+                    var valueProperty = item.GetType().GetProperty("Value");
+
+                    if (nameProperty != null && valueProperty != null) {
+                        var name = nameProperty.GetValue(item)?.ToString();
+                        var value = valueProperty.GetValue(item);
+                        if (name != null) {
+                            properties[name] = value;
+                        }
+                    }
+                }
+            }
+        }
+
+        return properties;
+    }
+
+
+
+
 
     public override string ToString() {
         return BuildTable();
