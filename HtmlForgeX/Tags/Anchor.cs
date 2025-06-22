6708680010e4834e419f9962393eaebe41bcbc9f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-22 19:07:40 +0200

    Add XML docs to core classes

--- HtmlForgeX/Tags/Anchor.cs
+++ HtmlForgeX/Tags/Anchor.cs
@@ -1,58 +1,119 @@
 namespace HtmlForgeX.Tags;
 
+/// <summary>
+/// Represents an HTML <c>&lt;a&gt;</c> tag.
+/// </summary>
 public class Anchor : HtmlTag {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Anchor"/> class.
+    /// </summary>
     public Anchor() : base("a") { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Anchor"/> class with a <c>href</c> value.
+    /// </summary>
+    /// <param name="hrefLink">URL for the <c>href</c> attribute.</param>
+    /// <param name="text">Optional inner text.</param>
     public Anchor(string hrefLink, string text = "") : base("a") {
         Attributes["href"] = hrefLink;
         Value(text);
     }
 
+    /// <summary>
+    /// Sets the <c>target</c> attribute.
+    /// </summary>
+    /// <param name="target">Target value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Target(string target) {
         Attributes["target"] = target;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>name</c> attribute.
+    /// </summary>
+    /// <param name="name">Name value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Name(string name) {
         Attributes["name"] = name;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>rel</c> attribute.
+    /// </summary>
+    /// <param name="rel">Relationship value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Rel(string rel) {
         Attributes["rel"] = rel;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>download</c> attribute.
+    /// </summary>
+    /// <param name="download">Download value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Download(string download) {
         Attributes["download"] = download;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>href</c> attribute.
+    /// </summary>
+    /// <param name="hrefLink">URL for the <c>href</c> attribute.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor HrefLink(string hrefLink) {
         Attributes["href"] = hrefLink;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>hreflang</c> attribute.
+    /// </summary>
+    /// <param name="hreflang">Language code.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor HrefLang(string hreflang) {
         Attributes["hreflang"] = hreflang;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>onclick</c> attribute.
+    /// </summary>
+    /// <param name="onClick">JavaScript code to execute.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor OnClick(string onClick) {
         Attributes["onclick"] = onClick;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>media</c> attribute.
+    /// </summary>
+    /// <param name="media">Media attribute value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Media(string media) {
         Attributes["media"] = media;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>ping</c> attribute.
+    /// </summary>
+    /// <param name="ping">Ping URL.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor Ping(string ping) {
         Attributes["ping"] = ping;
         return this;
     }
 
+    /// <summary>
+    /// Sets the <c>referrerpolicy</c> attribute.
+    /// </summary>
+    /// <param name="referrerPolicy">Referrer policy value.</param>
+    /// <returns>The current <see cref="Anchor"/> instance.</returns>
     public Anchor ReferrerPolicy(string referrerPolicy) {
         Attributes["referrerpolicy"] = referrerPolicy;
         return this;
