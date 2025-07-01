ca1971d982f28ade9930bd1a75ccc84e03e20f70
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 11:17:56 +0200

    Refactor TablerIcon class: move TablerIconElement to a new file and restore its implementation

---
+++ HtmlForgeX/Containers/Tabler/TablerIconElement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HtmlForgeX;
+
+public class TablerIconElement : Element {
+    public TablerIcon Icon { get; set; }
+    public Dictionary<string, string> IconStyle { get; set; } = new Dictionary<string, string>();
+
+    public TablerIconElement(TablerIcon icon) {
+        GlobalStorage.Libraries.TryAdd(Libraries.TablerIcon, 0);
+        Icon = icon;
+    }
+
+    public TablerIconElement Color(RGBColor color) {
+        IconStyle.Add("color", color.ToHex());
+        return this;
+    }
+
+    public TablerIconElement FontSize(int size) {
+        IconStyle.Add("font-size", $"{size}px");
+        return this;
+    }
+
+    public TablerIconElement StrokeWidth(double width) {
+        IconStyle.Add("stroke-width", $"{width}px");
+        return this;
+    }
+
+    public override string ToString() {
+        var icon = new HtmlTag("i");
+        icon.Class("ti " + Icon.Icon);
+        foreach (var property in IconStyle) {
+            icon.Style(property.Key, property.Value);
+        }
+        return icon.ToString();
+    }
+}
