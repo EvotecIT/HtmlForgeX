17cbe6c64bb77983d3f8a84c1d06a41d021063d9
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-13 09:58:39 +0200

    Fix WhereNotNull for value types

--- HtmlForgeX/Extensions/EnumerableExtensions.cs
+++ HtmlForgeX/Extensions/EnumerableExtensions.cs
@@ -2,7 +2,7 @@ namespace HtmlForgeX.Extensions;
 
 internal static class EnumerableExtensions
 {
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> source)
     {
         return source.Where(item => item is not null)!;
     }
