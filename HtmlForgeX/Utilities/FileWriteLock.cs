7641a2501b7159781d3c268d6a8aaac8bcea76bc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-16 20:24:25 +0200

    Dispose file semaphore when last Document is disposed

--- HtmlForgeX/Utilities/FileWriteLock.cs
+++ HtmlForgeX/Utilities/FileWriteLock.cs
@@ -1,5 +1,12 @@
 namespace HtmlForgeX;
 
 internal static class FileWriteLock {
-    internal static readonly System.Threading.SemaphoreSlim Semaphore = new(1, 1);
+    private static System.Threading.SemaphoreSlim _semaphore = new(1, 1);
+
+    internal static System.Threading.SemaphoreSlim Semaphore => _semaphore;
+
+    internal static void DisposeSemaphore() {
+        _semaphore.Dispose();
+        _semaphore = new System.Threading.SemaphoreSlim(1, 1);
+    }
 }
