822b977ec950e966637d8d68f3ac3e9c3150846b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-18 20:51:04 +0200

    Add XML docs for FontAwesomeBrands

--- HtmlForgeX/Icons/FontAwesome/FontAwesomeBrands.cs
+++ HtmlForgeX/Icons/FontAwesome/FontAwesomeBrands.cs
@@ -158,21 +158,39 @@ public enum FontAwesomeBrands {
     [Description("\uf2c6")]
     Telegram,
     
+    /// <summary>
+    /// Telegram Plane icon
+    /// </summary>
     [Description("\uf3fe")]
     TelegramPlane,
     
+    /// <summary>
+    /// Whatsapp icon
+    /// </summary>
     [Description("\uf232")]
     Whatsapp,
     
+    /// <summary>
+    /// Whatsapp Square icon
+    /// </summary>
     [Description("\uf40c")]
     WhatsappSquare,
     
+    /// <summary>
+    /// Discord icon
+    /// </summary>
     [Description("\uf392")]
     Discord,
     
+    /// <summary>
+    /// Skype icon
+    /// </summary>
     [Description("\uf1a7")]
     Skype,
     
+    /// <summary>
+    /// Tiktok icon
+    /// </summary>
     [Description("\uf6ed")]
     Tiktok,
     
@@ -183,105 +201,207 @@ public enum FontAwesomeBrands {
     [Description("\uf09b")]
     Github,
     
+    /// <summary>
+    /// Github Square icon
+    /// </summary>
     [Description("\uf092")]
     GithubSquare,
     
+    /// <summary>
+    /// Github Alt icon
+    /// </summary>
     [Description("\uf113")]
     GithubAlt,
     
+    /// <summary>
+    /// Gitlab icon
+    /// </summary>
     [Description("\uf296")]
     Gitlab,
     
+    /// <summary>
+    /// Git Square icon
+    /// </summary>
     [Description("\uf1d3")]
     GitSquare,
     
+    /// <summary>
+    /// Git icon
+    /// </summary>
     [Description("\uf1d2")]
     Git,
     
+    /// <summary>
+    /// Git Alt icon
+    /// </summary>
     [Description("\uf841")]
     GitAlt,
     
+    /// <summary>
+    /// Bitbucket icon
+    /// </summary>
     [Description("\uf18a")]
     Bitbucket,
     
+    /// <summary>
+    /// Html5 icon
+    /// </summary>
     [Description("\uf13b")]
     Html5,
     
+    /// <summary>
+    /// Css3 icon
+    /// </summary>
     [Description("\uf38b")]
     Css3,
     
+    /// <summary>
+    /// Css3 Alt icon
+    /// </summary>
     [Description("\uf13c")]
     Css3Alt,
     
+    /// <summary>
+    /// Javascript icon
+    /// </summary>
     [Description("\uf3b8")]
     Javascript,
     
+    /// <summary>
+    /// Javascript Square icon
+    /// </summary>
     [Description("\uf3b9")]
     JavascriptSquare,
     
+    /// <summary>
+    /// Js icon
+    /// </summary>
     [Description("\uf3b9")]
     Js,
     
+    /// <summary>
+    /// Js Square icon
+    /// </summary>
     [Description("\uf3ba")]
     JsSquare,
     
+    /// <summary>
+    /// Node icon
+    /// </summary>
     [Description("\uf41d")]
     Node,
     
+    /// <summary>
+    /// Node Js icon
+    /// </summary>
     [Description("\uf3d3")]
     NodeJs,
     
+    /// <summary>
+    /// Npm icon
+    /// </summary>
     [Description("\uf3d4")]
     Npm,
     
+    /// <summary>
+    /// Yarn icon
+    /// </summary>
     [Description("\uf420")]
     Yarn,
     
+    /// <summary>
+    /// Php icon
+    /// </summary>
     [Description("\uf4e8")]
     Php,
     
+    /// <summary>
+    /// Python icon
+    /// </summary>
     [Description("\uf41e")]
     Python,
     
+    /// <summary>
+    /// React icon
+    /// </summary>
     [Description("\uf4f5")]
     React,
     
+    /// <summary>
+    /// Angular icon
+    /// </summary>
     [Description("\uf41f")]
     Angular,
     
+    /// <summary>
+    /// Vuejs icon
+    /// </summary>
     [Description("\uf4f7")]
     Vuejs,
     
+    /// <summary>
+    /// Docker icon
+    /// </summary>
     [Description("\uf1a0")]
     Docker,
     
+    /// <summary>
+    /// Sass icon
+    /// </summary>
     [Description("\uf2b8")]
     Sass,
     
+    /// <summary>
+    /// Less icon
+    /// </summary>
     [Description("\uf41c")]
     Less,
     
+    /// <summary>
+    /// Gulp icon
+    /// </summary>
     [Description("\uf3b4")]
     Gulp,
     
+    /// <summary>
+    /// Grunt icon
+    /// </summary>
     [Description("\uf3b5")]
     Grunt,
     
+    /// <summary>
+    /// Figma icon
+    /// </summary>
     [Description("\uf7e1")]
     Figma,
     
+    /// <summary>
+    /// Behance icon
+    /// </summary>
     [Description("\uf1b1")]
     Behance,
     
+    /// <summary>
+    /// Behance Square icon
+    /// </summary>
     [Description("\uf1b4")]
     BehanceSquare,
     
+    /// <summary>
+    /// Steam icon
+    /// </summary>
     [Description("\uf1b5")]
     Steam,
     
+    /// <summary>
+    /// Steam Symbol icon
+    /// </summary>
     [Description("\uf1b6")]
     SteamSymbol,
     
+    /// <summary>
+    /// Steam Square icon
+    /// </summary>
     [Description("\uf3f6")]
     SteamSquare,
     
@@ -292,30 +412,57 @@ public enum FontAwesomeBrands {
     [Description("\uf179")]
     Apple,
     
+    /// <summary>
+    /// Windows icon
+    /// </summary>
     [Description("\uf17a")]
     Windows,
     
+    /// <summary>
+    /// Android icon
+    /// </summary>
     [Description("\uf17b")]
     Android,
     
+    /// <summary>
+    /// Linux icon
+    /// </summary>
     [Description("\uf17c")]
     Linux,
     
+    /// <summary>
+    /// Ubuntu icon
+    /// </summary>
     [Description("\uf415")]
     Ubuntu,
     
+    /// <summary>
+    /// Fedora icon
+    /// </summary>
     [Description("\uf316")]
     Fedora,
     
+    /// <summary>
+    /// Centos icon
+    /// </summary>
     [Description("\uf30a")]
     Centos,
     
+    /// <summary>
+    /// Debian icon
+    /// </summary>
     [Description("\uf313")]
     Debian,
     
+    /// <summary>
+    /// Suse icon
+    /// </summary>
     [Description("\uf6e1")]
     Suse,
     
+    /// <summary>
+    /// Redhat icon
+    /// </summary>
     [Description("\uf30e")]
     Redhat,
     
@@ -326,6 +473,9 @@ public enum FontAwesomeBrands {
     [Description("\uf270")]
     Amazon,
     
+    /// <summary>
+    /// Aws icon
+    /// </summary>
     [Description("\uf375")]
     Aws,
     
@@ -335,21 +485,39 @@ public enum FontAwesomeBrands {
     [Description("\uf1a3")]
     Google,
     
+    /// <summary>
+    /// Google Plus icon
+    /// </summary>
     [Description("\uf0d2")]
     GooglePlus,
     
+    /// <summary>
+    /// Google Plus G icon
+    /// </summary>
     [Description("\uf0d3")]
     GooglePlusG,
     
+    /// <summary>
+    /// Google Plus Square icon
+    /// </summary>
     [Description("\uf2b3")]
     GooglePlusSquare,
     
+    /// <summary>
+    /// Google Drive icon
+    /// </summary>
     [Description("\uf1a0")]
     GoogleDrive,
     
+    /// <summary>
+    /// Google Play icon
+    /// </summary>
     [Description("\uf3aa")]
     GooglePlay,
     
+    /// <summary>
+    /// Google Wallet icon
+    /// </summary>
     [Description("\uf3ab")]
     GoogleWallet,
     
@@ -359,329 +527,650 @@ public enum FontAwesomeBrands {
     [Description("\uf3d5")]
     Microsoft,
     
+    /// <summary>
+    /// Microsoft Teams icon
+    /// </summary>
     [Description("\uf19e")]
     MicrosoftTeams,
     
+    /// <summary>
+    /// Unity icon
+    /// </summary>
     [Description("\uf83c")]
     Unity,
     
+    /// <summary>
+    /// Dropbox icon
+    /// </summary>
     [Description("\uf19a")]
     Dropbox,
     
+    /// <summary>
+    /// Salesforce icon
+    /// </summary>
     [Description("\uf4ba")]
     Salesforce,
     
+    /// <summary>
+    /// Shopify icon
+    /// </summary>
     [Description("\uf835")]
     Shopify,
     
+    /// <summary>
+    /// Bootstrap icon
+    /// </summary>
     [Description("\uf7aa")]
     Bootstrap,
     
     // Payment Methods
+    /// <summary>
+    /// Cc Visa icon
+    /// </summary>
     [Description("\uf1f4")]
     CcVisa,
     
+    /// <summary>
+    /// Cc Mastercard icon
+    /// </summary>
     [Description("\uf1f5")]
     CcMastercard,
     
+    /// <summary>
+    /// Cc Amex icon
+    /// </summary>
     [Description("\uf1f3")]
     CcAmex,
     
+    /// <summary>
+    /// Cc Discover icon
+    /// </summary>
     [Description("\uf416")]
     CcDiscover,
     
+    /// <summary>
+    /// Cc Jcb icon
+    /// </summary>
     [Description("\uf24c")]
     CcJcb,
     
+    /// <summary>
+    /// Cc Diners Club icon
+    /// </summary>
     [Description("\uf24b")]
     CcDinersClub,
     
+    /// <summary>
+    /// Cc Paypal icon
+    /// </summary>
     [Description("\uf1f0")]
     CcPaypal,
     
+    /// <summary>
+    /// Cc Stripe icon
+    /// </summary>
     [Description("\uf1ed")]
     CcStripe,
     
+    /// <summary>
+    /// Cc Apple Pay icon
+    /// </summary>
     [Description("\uf1ee")]
     CcApplePay,
     
+    /// <summary>
+    /// Cc Amazon Pay icon
+    /// </summary>
     [Description("\uf3d1")]
     CcAmazonPay,
     
+    /// <summary>
+    /// Google Pay icon
+    /// </summary>
     [Description("\uf1f1")]
     GooglePay,
     
+    /// <summary>
+    /// Apple Pay icon
+    /// </summary>
     [Description("\uf415")]
     ApplePay,
     
+    /// <summary>
+    /// Bitcoin icon
+    /// </summary>
     [Description("\uf42e")]
     Bitcoin,
     
+    /// <summary>
+    /// Btc icon
+    /// </summary>
     [Description("\uf379")]
     Btc,
     
+    /// <summary>
+    /// Ethereum icon
+    /// </summary>
     [Description("\uf42e")]
     Ethereum,
     
     // Communication Platforms
+    /// <summary>
+    /// Slack icon
+    /// </summary>
     [Description("\uf198")]
     Slack,
     
+    /// <summary>
+    /// Slack Hash icon
+    /// </summary>
     [Description("\uf3ef")]
     SlackHash,
     
+    /// <summary>
+    /// Trello icon
+    /// </summary>
     [Description("\uf263")]
     Trello,
     
+    /// <summary>
+    /// Jira icon
+    /// </summary>
     [Description("\uf27d")]
     Jira,
     
+    /// <summary>
+    /// Confluence icon
+    /// </summary>
     [Description("\uf77d")]
     Confluence,
     
+    /// <summary>
+    /// Atlassian icon
+    /// </summary>
     [Description("\uf5ef")]
     Atlassian,
     
     // Entertainment
+    /// <summary>
+    /// Spotify icon
+    /// </summary>
     [Description("\uf2b2")]
     Spotify,
     
+    /// <summary>
+    /// Soundcloud icon
+    /// </summary>
     [Description("\uf001")]
     Soundcloud,
     
+    /// <summary>
+    /// Vimeo icon
+    /// </summary>
     [Description("\uf27e")]
     Vimeo,
     
+    /// <summary>
+    /// Vimeo V icon
+    /// </summary>
     [Description("\uf194")]
     VimeoV,
     
+    /// <summary>
+    /// Vimeo Square icon
+    /// </summary>
     [Description("\uf237")]
     VimeoSquare,
     
     // Other Popular Brands
+    /// <summary>
+    /// Adobe icon
+    /// </summary>
     [Description("\uf099")]
     Adobe,
     
+    /// <summary>
+    /// Adobe Reader icon
+    /// </summary>
     [Description("\uf4b6")]
     AdobeReader,
     
+    /// <summary>
+    /// Airbnb icon
+    /// </summary>
     [Description("\uf8c5")]
     Airbnb,
     
+    /// <summary>
+    /// Alipay icon
+    /// </summary>
     [Description("\uf36e")]
     Alipay,
     
+    /// <summary>
+    /// Adn icon
+    /// </summary>
     [Description("\uf369")]
     Adn,
     
+    /// <summary>
+    /// Blogger icon
+    /// </summary>
     [Description("\uf642")]
     Blogger,
     
+    /// <summary>
+    /// Blogger B icon
+    /// </summary>
     [Description("\uf37f")]
     BloggerB,
     
+    /// <summary>
+    /// Bluetooth icon
+    /// </summary>
     [Description("\uf293")]
     Bluetooth,
     
+    /// <summary>
+    /// Bluetooth B icon
+    /// </summary>
     [Description("\uf294")]
     BluetoothB,
     
+    /// <summary>
+    /// Chrome icon
+    /// </summary>
     [Description("\uf268")]
     Chrome,
     
+    /// <summary>
+    /// Firefox icon
+    /// </summary>
     [Description("\uf269")]
     Firefox,
     
+    /// <summary>
+    /// Edge icon
+    /// </summary>
     [Description("\uf28a")]
     Edge,
     
+    /// <summary>
+    /// Internet Explorer icon
+    /// </summary>
     [Description("\uf267")]
     InternetExplorer,
     
+    /// <summary>
+    /// Opera icon
+    /// </summary>
     [Description("\uf2a8")]
     Opera,
     
+    /// <summary>
+    /// Safari icon
+    /// </summary>
     [Description("\uf23a")]
     Safari,
     
+    /// <summary>
+    /// Quora icon
+    /// </summary>
     [Description("\uf287")]
     Quora,
     
+    /// <summary>
+    /// Raspberry icon
+    /// </summary>
     [Description("\uf4ec")]
     Raspberry,
     
     
+    /// <summary>
+    /// Ravelry icon
+    /// </summary>
     [Description("\uf41b")]
     Ravelry,
     
+    /// <summary>
+    /// Reacteurope icon
+    /// </summary>
     [Description("\uf4f6")]
     Reacteurope,
     
+    /// <summary>
+    /// Shopware icon
+    /// </summary>
     [Description("\uf3e2")]
     Shopware,
     
+    /// <summary>
+    /// Sourcetree icon
+    /// </summary>
     [Description("\uf681")]
     Sourcetree,
     
+    /// <summary>
+    /// Stackoverflow icon
+    /// </summary>
     [Description("\uf1bc")]
     Stackoverflow,
     
+    /// <summary>
+    /// Stripe icon
+    /// </summary>
     [Description("\uf6dc")]
     Stripe,
     
+    /// <summary>
+    /// Stumble Upon icon
+    /// </summary>
     [Description("\uf1d8")]
     StumbleUpon,
     
+    /// <summary>
+    /// Stumble Upon Circle icon
+    /// </summary>
     [Description("\uf1d9")]
     StumbleUponCircle,
     
+    /// <summary>
+    /// Stripe S icon
+    /// </summary>
     [Description("\uf6dd")]
     StripeS,
     
+    /// <summary>
+    /// Teamspeak icon
+    /// </summary>
     [Description("\uf1bb")]
     Teamspeak,
     
+    /// <summary>
+    /// The Red Yeti icon
+    /// </summary>
     [Description("\uf6d5")]
     TheRedYeti,
     
+    /// <summary>
+    /// Themeco icon
+    /// </summary>
     [Description("\uf6d6")]
     Themeco,
     
+    /// <summary>
+    /// Themeisle icon
+    /// </summary>
     [Description("\uf6d7")]
     Themeisle,
     
+    /// <summary>
+    /// Think Peaks icon
+    /// </summary>
     [Description("\uf731")]
     ThinkPeaks,
     
+    /// <summary>
+    /// Trade Federation icon
+    /// </summary>
     [Description("\uf6de")]
     TradeFederation,
     
+    /// <summary>
+    /// Uber icon
+    /// </summary>
     [Description("\uf407")]
     Uber,
     
+    /// <summary>
+    /// Umbraco icon
+    /// </summary>
     [Description("\uf7e0")]
     Umbraco,
     
+    /// <summary>
+    /// Uncharted icon
+    /// </summary>
     [Description("\uf8cc")]
     Uncharted,
     
+    /// <summary>
+    /// Uniregistry icon
+    /// </summary>
     [Description("\uf403")]
     Uniregistry,
     
+    /// <summary>
+    /// Unsplash icon
+    /// </summary>
     [Description("\uf8e8")]
     Unsplash,
     
+    /// <summary>
+    /// Untappd icon
+    /// </summary>
     [Description("\uf2e0")]
     Untappd,
     
+    /// <summary>
+    /// Ups icon
+    /// </summary>
     [Description("\uf408")]
     Ups,
     
+    /// <summary>
+    /// Usb icon
+    /// </summary>
     [Description("\uf3cd")]
     Usb,
     
+    /// <summary>
+    /// Usps icon
+    /// </summary>
     [Description("\uf409")]
     Usps,
     
+    /// <summary>
+    /// Vaadin icon
+    /// </summary>
     [Description("\uf7e1")]
     Vaadin,
     
+    /// <summary>
+    /// Viacoin icon
+    /// </summary>
     [Description("\uf221")]
     Viacoin,
     
+    /// <summary>
+    /// Viadeo icon
+    /// </summary>
     [Description("\uf2a9")]
     Viadeo,
     
+    /// <summary>
+    /// Viadeo Square icon
+    /// </summary>
     [Description("\uf2aa")]
     ViadeoSquare,
     
+    /// <summary>
+    /// Viber icon
+    /// </summary>
     [Description("\uf40a")]
     Viber,
     
+    /// <summary>
+    /// Vine icon
+    /// </summary>
     [Description("\uf1ca")]
     Vine,
     
+    /// <summary>
+    /// Vk icon
+    /// </summary>
     [Description("\uf189")]
     Vk,
     
+    /// <summary>
+    /// Vnv icon
+    /// </summary>
     [Description("\uf2ca")]
     Vnv,
     
+    /// <summary>
+    /// Watchman Monitoring icon
+    /// </summary>
     [Description("\uf77e")]
     WatchmanMonitoring,
     
+    /// <summary>
+    /// Waze icon
+    /// </summary>
     [Description("\uf5dc")]
     Waze,
     
+    /// <summary>
+    /// Weebly icon
+    /// </summary>
     [Description("\uf18e")]
     Weebly,
     
+    /// <summary>
+    /// Weibo icon
+    /// </summary>
     [Description("\uf1e9")]
     Weibo,
     
+    /// <summary>
+    /// Weixin icon
+    /// </summary>
     [Description("\uf5cc")]
     Weixin,
     
+    /// <summary>
+    /// Whats App icon
+    /// </summary>
     [Description("\uf232")]
     WhatsApp,
     
+    /// <summary>
+    /// Whmcs icon
+    /// </summary>
     [Description("\uf193")]
     Whmcs,
     
+    /// <summary>
+    /// Wikipedia W icon
+    /// </summary>
     [Description("\uf1eb")]
     WikipediaW,
     
+    /// <summary>
+    /// Wix icon
+    /// </summary>
     [Description("\uf412")]
     Wix,
     
+    /// <summary>
+    /// Wizards Of The Coast icon
+    /// </summary>
     [Description("\uf5cf")]
     WizardsOfTheCoast,
     
+    /// <summary>
+    /// Wolf Pack Battalion icon
+    /// </summary>
     [Description("\uf38f")]
     WolfPackBattalion,
     
+    /// <summary>
+    /// Wordpress icon
+    /// </summary>
     [Description("\uf19a")]
     Wordpress,
     
+    /// <summary>
+    /// Wordpress Simple icon
+    /// </summary>
     [Description("\uf297")]
     WordpressSimple,
     
+    /// <summary>
+    /// Wpbeginner icon
+    /// </summary>
     [Description("\uf2de")]
     Wpbeginner,
     
+    /// <summary>
+    /// Wpexplorer icon
+    /// </summary>
     [Description("\uf298")]
     Wpexplorer,
     
+    /// <summary>
+    /// Wpforms icon
+    /// </summary>
     [Description("\uf2e1")]
     Wpforms,
     
+    /// <summary>
+    /// Wpressr icon
+    /// </summary>
     [Description("\uf8dd")]
     Wpressr,
     
+    /// <summary>
+    /// Xing icon
+    /// </summary>
     [Description("\uf168")]
     Xing,
     
+    /// <summary>
+    /// Xing Square icon
+    /// </summary>
     [Description("\uf169")]
     XingSquare,
     
+    /// <summary>
+    /// Ycombinator icon
+    /// </summary>
     [Description("\uf7e3")]
     Ycombinator,
     
+    /// <summary>
+    /// Yahoo icon
+    /// </summary>
     [Description("\uf19e")]
     Yahoo,
     
+    /// <summary>
+    /// Yammer icon
+    /// </summary>
     [Description("\uf414")]
     Yammer,
     
+    /// <summary>
+    /// Yandex icon
+    /// </summary>
     [Description("\uf413")]
     Yandex,
     
+    /// <summary>
+    /// Yandex International icon
+    /// </summary>
     [Description("\uf1a9")]
     YandexInternational,
     
+    /// <summary>
+    /// Yelp icon
+    /// </summary>
     [Description("\uf23b")]
     Yelp,
     
+    /// <summary>
+    /// Zhihu icon
+    /// </summary>
     [Description("\uf63f")]
     Zhihu
 }
