2773f53ab628db2afb804d7e291286d9ccc0e4bf
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-23 09:15:20 +0200

    Add VisNetwork attribute tests and example

---
+++ HtmlForgeX.Examples/Containers/VisNetworkIdSizeExample.cs
@@ -0,0 +1,32 @@
+namespace HtmlForgeX.Examples.Containers;
+
+internal class VisNetworkIdSizeExample
+{
+    public static void Demo(bool openInBrowser = false)
+    {
+        HelpersSpectre.PrintTitle("VisNetwork Id & Size Demo");
+
+        using var document = new Document
+        {
+            Head = { Title = "VisNetwork Id & Size Demo", Author = "HtmlForgeX" },
+            LibraryMode = LibraryMode.Online,
+            ThemeMode = ThemeMode.Light
+        };
+
+        document.Body.Page(page =>
+        {
+            page.DiagramNetwork(network =>
+            {
+                network
+                    .WithId("exampleNetwork")
+                    .WithSize("100%", "300px");
+
+                network.AddNode(1, node => node.WithLabel("First"));
+                network.AddNode(2, node => node.WithLabel("Second"));
+                network.AddEdge(1, 2);
+            });
+        });
+
+        document.Save("VisNetworkIdSizeDemo.html", openInBrowser);
+    }
+}
